479bae4db717792b34752245dbe5cb0f50bdc6b6
JLMosconi <xlost.memoriesx@gmail.com>
2016-10-04 13:21:44 -0300

    cambios contact us

--- src/components/LandingContactForm.js
+++ src/components/LandingContactForm.js
@@ -7,14 +7,44 @@ class LandingContactForm extends Component {
     render(){
         const { language } = this.props;
         return(
-            <div className="row contactFormContainer">
-                <div className="col-md-6">
-                    <input type="text" className="contact" placeholder={i18n[language].name}/>
-                    <input type="text" className="contact" placeholder={i18n[language].email}/>
-                    <input type="text" className="contact" placeholder={i18n[language].phone}/>
-                </div>
-                <div className="col-md-6">
 
+            <div className="row">
+                <div className="col-md-6 col-md-offset-3">
+                    <div className="well well-sm">
+                    <form className="form-horizontal" action="" method="post">
+                        <fieldset>
+                            <legend className="text-center">
+                                <h4>{i18n[language].contactUs}</h4>
+                            </legend>
+
+                            <div className="form-group">
+                            <label className="col-md-3 control-label">{i18n[language].name}</label>
+                                <div className="col-md-9">
+                                    <input id="name" name="name" type="text" placeholder={i18n[language].name} className="form-control" />
+                                </div>
+                            </div>
+                    
+                            <div className="form-group">
+                            <label className="col-md-3 control-label">{i18n[language].email}</label>
+                                <div className="col-md-9">
+                                    <input id="email" name="email" type="text" placeholder={i18n[language].email} className="form-control"/>
+                                </div>
+                            </div>
+                    
+                            <div className="form-group">
+                                <label className="col-md-3 control-label">{i18n[language].message}</label>
+                                <div className="col-md-9">
+                                    <textarea className="form-control" id="message" name="message" placeholder={i18n[language].message} rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="col-md-12 text-right">
+                                    <button type="submit" className="btn btn-primary btn-lg">{i18n[language].submit}</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                    </div>
                 </div>
             </div>
         )
--- src/components/LandingHeader.js
+++ src/components/LandingHeader.js
@@ -1,21 +1,13 @@
 import React from 'react';
-
 import * as i18n from './LandingHeaderI18n.json';
+import './Landing.css'
 
 export default function LandingHeader({language}){
-    console.log(i18n);
     return(
-        <div className="row">
-            <div className="col-md-4">
-                <h2>
-                    {i18n[language].title}
-                </h2>
-            </div>
-            <div className="col-md-8">
-                <h2>
-                    Hello 2
-                </h2>
-            </div>
+        <div className="jumbotron">
+            <h1>Hello, ReactJS!</h1>
+            <p>{i18n[language].title}</p>
+            <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
         </div>
     )
 }
